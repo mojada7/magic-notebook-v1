b33ba61395e09575f88168c018ca0ba852b4cce3
mohammad <M@>
2023-10-02 23:19:41 +0330

    anymatter

--- src/components/searchWord/index.tsx
+++ src/components/searchWord/index.tsx
@@ -16,7 +16,7 @@ function SearchWord({words}:any) {
 
 
 
-    let sRes = words.total.filter(w=>{
+    let sRes = words.total.filter((w:any)=>{
       return pattern.test(w.english)
     })
 
@@ -34,7 +34,7 @@ function SearchWord({words}:any) {
       {
         (serachRes.length>0)?(
           <div className='w-[17rem] h-[10rem] md:w-[24rem] overflow-hidden z-50 lg:w-[31rem]'>
-            {serachRes.map(w=>{
+            {serachRes.map((w : any)=>{
               return(
                 <Link key={w.english} href={`/mydictionary/${w.english[0]}`} >
                   <div  className='w-full bg-white ps-4 pt-[1px] z-50 border-[2px] border-sky-200 hover:bg-sky-100 h-[2rem]'>
